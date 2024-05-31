db33251a57888d1006c131e39b79ed1890c6088d
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-05-31 12:59:28 +0300

    Add files via upload

--- Создарие потоков.py
+++ Создарие потоков.py
@@ -7,6 +7,7 @@ def passage(iter_obj):
         print(i)
         time.sleep(1)
 
+
 t1 = threading.Thread(target=passage, args=(range(1, 11),))
 t2 = threading.Thread(target=passage, kwargs=dict(iter_obj='abcdefghij'))
 locker = threading.RLock()
