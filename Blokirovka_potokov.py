cc57037b3bdcd65c0fb6fe1865df397815c0c290
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-06-09 21:41:29 +0300

    Add files via upload

--- Blokirovka_potokov.py
+++ Blokirovka_potokov.py
@@ -4,34 +4,38 @@
 # для пополнения и снятия денег. Необходимо использовать механизм блокировки,
 # чтобы избежать проблемы гонок (race conditions) при модификации общего ресурса.
 # Пример работы
+import threading
+
+
 class BankAccount:
-    def deposit_task(account, amount):
-         for _ in range(5):
-             account.deposit(amount)
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.lock = threading.Lock()
+
+    def deposit(self, amount):
+        with self.lock:
+            self.balance += amount
+            print(f"Deposited {amount}, new balance is {self.balance}")
+
+    def withdraw(self, amount):
+        with self.lock:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Withdrew {amount}, new balance is {self.balance}")
+            else:
+                print("Недостаточно средств на счету")
 
 
-    def withdraw_task(account, amount):
-        for _ in range(5):
-            account.withdraw(amount)
-            account = BankAccount()
+account = BankAccount(balance=1000)
+threads = []
 
-deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
-withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+for i in range(5):
+    deposit_thread = threading.Thread(target=account.deposit, args=(100,))
+    withdraw_thread = threading.Thread(target=account.withdraw, args=(150,))
+    threads.extend([deposit_thread, withdraw_thread])
 
-deposit_thread.start()
-withdraw_thread.start()
+for thread in threads:
+    thread.start()
 
-deposit_thread.join()
-withdraw_thread.join()
-
-# Вывод в консоль:
-# Deposited 100, new balance is 1100
-# Deposited 100, new balance is 1200
-# Deposited 100, new balance is 1300
-# Deposited 100, new balance is 1400
-# Deposited 100, new balance is 1500
-# Withdrew 150, new balance is 1350
-# Withdrew 150, new balance is 1200
-# Withdrew 150, new balance is 1050
-# Withdrew 150, new balance is 900
-# Withdrew 150, new balance is 750
+for thread in threads:
+    thread.join()
