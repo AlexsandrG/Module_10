eebfd3e1020516556fdd770485ec025146d958ac
AlexsandrG <167127179+AlexsandrG@users.noreply.github.com>
2024-06-10 15:57:05 +0300

    Add files via upload

---
+++ ochered2.py
@@ -0,0 +1,57 @@
+import threading
+import time
+from queue import Queue
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.is_busy = False
+
+class Customer(threading.Thread):
+    def __init__(self, number, table, cafe):
+        threading.Thread.__init__(self)
+        self.number = number
+        self.table = table
+        self.cafe = cafe
+
+    def run(self):
+        print(f"Посетитель номер {self.number} прибыл.")
+        if self.table is None:
+            if not any([t.is_busy for t in self.cafe.tables]):
+                self.table = self.cafe.tables.pop(0)
+                self.table.is_busy = True
+                print(f"Посетитель номер {self.number} сел за стол {self.table.number}. (начало обслуживания)")
+                time.sleep(5)
+                self.table.is_busy = False
+                print(f"Посетитель номер {self.number} покушал и ушёл. (конец обслуживания)")
+            else:
+                print(f"Посетитель номер {self.number} ожидает свободный стол. (помещение в очередь)")
+                self.cafe.queue.put(self)
+        else:
+            time.sleep(5)
+            print(f"Посетитель номер {self.number} покушал и ушёл. (конец обслуживания)")
+
+class Cafe:
+    def __init__(self, tables):
+        self.tables = tables
+        self.queue = Queue()
+
+    def customer_arrival(self):
+        for i in range(1, 21):
+            Customer(i, None, self).start()
+
+# Создаем столики в кафе
+table1 = Table(1)
+table2 = Table(2)
+table3 = Table(3)
+tables = [table1, table2, table3]
+
+# Инициализируем кафе
+cafe = Cafe(tables)
+
+# Запустим поток для прибытия посетителей
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+
+# Ожидаем завершения работы потока прибытия посетителей
+customer_arrival_thread.join()
